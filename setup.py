b567cc82e649264ce0aec292a85403b1d866aff2
AndreCNF <andrecnf@gmail.com>
2020-06-23 03:43:29 +0100

    Updated dependencies one more time (third time's a charm)

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ setup(
     packages=['data_utils'],
     package_data={},
     install_requires=[
-        'comet-ml==3.*,>=3.1.2', 'dask==2.*,>=2.5.0',
-        'distributed==2.*,>=2.5.0', 'everett==1.*,>=1.0.2',
+        'comet-ml==3.*,>=3.1.10,>=3.1.2', 'dash==1.*,>=1.12.0',
+        'dask==2.*,>=2.5.0', 'distributed==2.*,>=2.5.0', 'everett==1.*,>=1.0.2',
         'fsspec==0.*,>=0.6.0', 'modin==0.*,>=0.7.2', 'pandas==1.0.1',
-        'pdoc3==0.*,>=0.7.2', 'pillow==7.*,>=7.0.0', 'portray==1.*,>=1.3.0',
-        'ray==0.8.0', 'sklearn==0.*,>=0.0.0', 'sphinx==2.*,>=2.2.0',
-        'torch==1.*,>=1.3.0', 'tqdm==4.*,>=4.43.0'
+        'pdoc3==0.*,>=0.7.2', 'pillow==7.*,>=7.0.0', 'plotly==4.*,>=4.8.0',
+        'portray==1.*,>=1.3.0', 'ray', 'sklearn==0.*,>=0.0.0',
+        'sphinx==2.*,>=2.2.0', 'torch==1.*,>=1.3.0', 'tqdm==4.*,>=4.43.0'
     ],
     extras_require={'dev': ['autopep8==1.*,>=1.4.0', 'pytest==3.*,>=3.0.0']},
 )
